00302232c35010b6a16307c52be4f9785ad5d023
Fran <franciscojaviermontielnoguera@gmail.com>
2024-09-30 12:08:33 +0200

    Primera parte hecha

--- noteApp/src/app/app.component.ts
+++ noteApp/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { NotesListComponent } from '../notes-list/notes-list.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
